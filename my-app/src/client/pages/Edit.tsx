777f9d45c2dfa1e0eb55a3890421b6e0f5e08863
2018302110301@whu.edu.cn <2018302110301@whu.edu.cn>
2021-11-25 19:09:41 +0800

    feat: 新增编辑情书预览卡片

--- my-app/src/client/pages/Edit.tsx
+++ my-app/src/client/pages/Edit.tsx
@@ -1,5 +1,5 @@
-import { AppstoreOutlined, ContainerOutlined, MailOutlined } from "@ant-design/icons"
-import { Tabs } from "antd"
+import { AppstoreOutlined, ContainerOutlined, DeleteOutlined, EditOutlined, MailOutlined } from "@ant-design/icons"
+import { Card, Popconfirm, Tabs, message } from "antd"
 import styled from "styled-components"
 
 const EditPageContainer = styled.div`
@@ -16,9 +16,9 @@ const EditPageAsider = styled.div`
 `
 
 const NewButton = styled.button`
-    background-color: blue;
+    background-color: #4386f5;
     color: white;
-    width: 84px;
+    width: 110px;
     height: 36px;
     border-radius: 8px;
     margin: 8px 24px;
@@ -28,23 +28,54 @@ const EditPageContent = styled.div`
 
 `
 
-const EditPageContentHeader = styled.div`
-
-`
-
-const EditOption = styled.div`
-    display: flex;
-`
-
-const EditOptionText = styled.div`
-    color: black;
-`
-
-const EditOptionIcon = styled.div`
-    color: black;
-`
-
 const {TabPane} = Tabs;
+
+const PreviewCardsContainer = styled.div`
+    display: flex;
+
+`
+
+const PreviewCardContainer = styled.div`
+    height: 20%;
+    width: 20%;
+`
+
+const PreviewCards: React.FC = () => {
+    return (
+        <PreviewCardContainer>
+            <Card
+                cover={
+                    <img
+                        alt=""
+                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    />
+                }
+                actions={[
+                    <EditOutlined key="edit" />,
+                    <Popconfirm 
+                        title="确定要删除这封情书吗"
+                        onConfirm={
+                            ()=> {
+                                message.success('删除的情书可以在回收站找到哟')
+                            }
+                        }
+                        onCancel={
+                            () => {
+                                message.error('已取消')
+                            }
+                        }
+                        okText="删除"
+                        cancelText="取消"
+                    >
+                        <DeleteOutlined key="delete"/>
+                    </Popconfirm>
+                    
+                ]}
+            />
+        </PreviewCardContainer>
+        
+    )
+}
 
 export const EditPage: React.FC = () => {
     return (
@@ -59,7 +90,10 @@ export const EditPage: React.FC = () => {
                                 我制作的情书
                             </span>
                         }
-                        key="1"></TabPane>
+                        key="1">
+                            文件
+                            <PreviewCards></PreviewCards>
+                        </TabPane>
                     <TabPane 
                         tab={
                             <span>
