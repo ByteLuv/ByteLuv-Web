7f8452fa4520dbcc871e7da4e64df624ed1c97bb
2018302110301@whu.edu.cn <2018302110301@whu.edu.cn>
2021-11-25 16:53:40 +0800

    fix: make ts happy

--- my-app/src/client/pages/Schedule.tsx
+++ my-app/src/client/pages/Schedule.tsx
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import moment from "moment";
-import { Badge, Calendar } from "antd";
+import { Badge, Calendar, Popover } from "antd";
+
+type ScheduleItemStatusType = "warning" | "success" | "error" | "processing" | "default"
+
+interface ScheduleItem {
+    type: ScheduleItemStatusType;
+    content: string;
+}
 
 const SchedulePageContainer = styled.div`
     height: 100%;
@@ -10,57 +17,63 @@ const SchedulePageContainer = styled.div`
 const ScheduleContainer = styled.div`
     
 `
-type ScheduleItemStatusType = "warning" | "success" | "error" | "processing" | "default"
-
-interface ScheduleItem {
-    type: ScheduleItemStatusType;
-    content: string;
-}
 
 const Schedule: React.FC = () => {
     function getListData(val: moment.Moment) {
         let listData: ScheduleItem[] = [];
         switch (val.date()) {
-          case 8:
+            case 8:
             listData = [
-              { type: 'warning', content: 'This is warning event.' },
-              { type: 'success', content: 'This is usual event.' },
+                { type: 'warning', content: 'This is warning event.' },
+                { type: 'success', content: 'This is usual event.' },
             ];
             break;
-          case 10:
+            case 10:
             listData = [
-              { type: 'warning', content: 'This is warning event.' },
-              { type: 'success', content: 'This is usual event.' },
-              { type: 'error', content: 'This is error event.' },
+                { type: 'warning', content: 'This is warning event.' },
+                { type: 'success', content: 'This is usual event.' },
+                { type: 'error', content: 'This is error event.' },
             ];
             break;
-          case 15:
+            case 15:
             listData = [
-              { type: 'warning', content: 'This is warning event' },
-              { type: 'success', content: 'This is very long usual event。。....' },
-              { type: 'error', content: 'This is error event 1.' },
-              { type: 'error', content: 'This is error event 2.' },
-              { type: 'error', content: 'This is error event 3.' },
-              { type: 'error', content: 'This is error event 4.' },
+                { type: 'warning', content: 'This is warning event' },
+                { type: 'success', content: 'This is very long usual event。。....' },
+                { type: 'error', content: 'This is error event 1.' },
+                { type: 'error', content: 'This is error event 2.' },
+                { type: 'error', content: 'This is error event 3.' },
+                { type: 'error', content: 'This is error event 4.' },
             ];
             break;
-          default:
+            default:
         }
         return listData;
     }
+
     function dateCellRender(val: moment.Moment) {
         const listData = getListData(val);
-        return (
-          <div>
-            {listData.map((item: ScheduleItem) => (
-              <div key={item.content}>
-                <Badge status={item.type} text={item.content} />
-              </div>
-            ))}
-          </div>
-        );
-      }
-      
+        const tabList = [
+            {
+                key: 'tab1',
+                tab: '你的日程'
+            },
+            {
+                key: 'tab2',
+                tab: 'Ta的日程'
+            },        
+            {
+                key: 'tab3',
+                tab: '共同日程'
+            }
+        ]
+
+        return listData.map((item: ScheduleItem) => (
+                <div key={item.content}>
+                    <Badge status={item.type} text={item.content} />
+                </div>
+        ))
+    }
+    
     function getMonthData(val: moment.Moment) {
         if (val.month() === 8) {
             return 1394;
@@ -80,7 +93,6 @@ const Schedule: React.FC = () => {
         <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender}></Calendar>
     )
 }
-
 
 export const SchedulePage: React.FC = () => {
     return (
